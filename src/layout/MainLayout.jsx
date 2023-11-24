70bbce37fe48bed329032c1533ba5357e48a21a2
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-24 20:46:24 +0600

    Navbar added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <>
-      MainLayout
+      <Navbar />
       <Outlet />
     </>
   );
---
+++ src/pages/Shared/SectionContainer/SectionContainer.jsx
@@ -0,0 +1,9 @@
+const SectionContainer = ({ children }) => {
+  return (
+    <div className="max-w-screen-xl px-4 sm:px-6 lg:px-8 mx-auto">
+      {children}
+    </div>
+  );
+};
+
+export default SectionContainer;
