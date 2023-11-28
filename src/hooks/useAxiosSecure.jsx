1747dd5c9c1a92d3ac381057a103e2e08602a7c2
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-28 22:04:38 +0600

    Unauthorized and forbidden access omitted

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,35 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const accessToken = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${accessToken}`;
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  axiosSecure.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+    }
+  );
+
   return axiosSecure;
 };
 
