57dcf9c026b13e90e8172ceb89c4335d85a004c1
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-28 06:13:56 +0600

    Skeleton added in camp details page when loading

--- src/pages/CampDetails/CampDetails.jsx
+++ src/pages/CampDetails/CampDetails.jsx
@@ -35,27 +35,35 @@ const CampDetails = () => {
       <SectionContainer>
         <div className="pb-20 space-y-4">
           <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center mb-4 sm:mb-6 mt-5 sm:mt-8 max-w-lg md:max-w-2xl lg:max-w-3xl mx-auto">
-            {camp_name}
+            {camp_name || <Skeleton className="h-14" />}
           </h1>
-          <img
-            className="max-w-md md:max-w-xl lg:max-w-2xl mx-auto pt-4"
-            src={image}
-            alt=""
-          />
-          <h4 className="sm:text-lg font-medium">
-            Specialized Service: {service_provided}
-          </h4>
-          <p className="sm:text-lg font-medium">
-            Healthcare Professional: {healthcare_professionals}
-          </p>
-          <p>Target Audience: {target_audience}</p>
-          <p>Starting Date: {starting_date}</p>
-          <p>Time: {time} (24 hour format)</p>
-          <p className="font-medium">Registration Fee: ${fees}</p>
-          <p className="text-gray-800 pb-6">{description}</p>
-          <button className="bg-violet-600 hover:bg-violet-800 text-white md:text-lg font-medium rounded-lg px-8 py-2.5">
-            Join Camp
-          </button>
+          <div className="max-w-md md:max-w-xl lg:max-w-2xl mx-auto pt-4">
+            {image ? (
+              <img className="w-full" src={image} alt="" />
+            ) : (
+              <Skeleton className="h-56" />
+            )}
+          </div>
+          {data ? (
+            <>
+              <h4 className="sm:text-lg font-medium pt-8">
+                Specialized Service: {service_provided}
+              </h4>
+              <p className="sm:text-lg font-medium">
+                Healthcare Professional: {healthcare_professionals}
+              </p>
+              <p>Target Audience: {target_audience}</p>
+              <p>Starting Date: {starting_date}</p>
+              <p>Time: {time} (24 hour format)</p>
+              <p className="font-medium">Registration Fee: ${fees}</p>
+              <p className="text-gray-800 pb-6">{description}</p>
+              <button className="bg-violet-600 hover:bg-violet-800 text-white md:text-lg font-medium rounded-lg px-8 py-2.5">
+                Join Camp
+              </button>
+            </>
+          ) : (
+            <Skeleton className="mt-4" count={10} />
+          )}
         </div>
       </SectionContainer>
     </>
